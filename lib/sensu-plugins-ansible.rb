a899e4aa91c4e2c7039ccdd89dcd54c33a436783
Matt Jones <mattjones@yieldbot.com>
2015-06-02 10:52:12 -0400

    update repo

--- lib/sensu-plugins-ansible.rb
+++ lib/sensu-plugins-ansible.rb
@@ -1,15 +1,2 @@
 
 require 'sensu-plugins-ansible/version'
-
-# Load the defaults
-
-#
-# Default class
-#
-module SensuPluginsAnsible
-  class << self
-  end
-
-  class << self
-  end
-end
--- lib/sensu-plugins-ansible/version.rb
+++ lib/sensu-plugins-ansible/version.rb
@@ -1,28 +1,11 @@
 require 'json'
 
-# encoding: utf-8
 module SensuPluginsAnsible
-  # This defines the version of the gem
   module Version
     MAJOR = 0
     MINOR = 0
     PATCH = 2
 
     VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
-
-    NAME   = 'sensu-plugins-ansible'
-    BANNER = "#{NAME} v%s"
-
-    module_function
-
-    def version
-      format(BANNER, VER_STRING)
-    end
-
-    def json_version
-      {
-        'version' => VER_STRING
-      }.to_json
-    end
   end
 end
